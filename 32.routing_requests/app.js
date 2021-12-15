3adf8800e3102868b8f3f88dedfab751ecb3d1f2
Denis Rotaru <taypeh@gmail.com>
2021-12-15 23:12:49 +0200

    task-3

--- 32.routing_requests/app.js
+++ 32.routing_requests/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res) {
 // }
@@ -7,20 +8,44 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<body>main page</body>');
-        res.write('</head>');
-        res.end();
+        res.write('<body><form action="message" method="POST"><input type="text" name="message" /><input type="text" name="addMessage" /><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(parsedBody);
+            // fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                console.log('some some');
+                console.log(err);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            })
+        });
+
     }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body>Hello from my Nodejs</body>');
-    res.write('</head>');
+    res.write('</html>');
     res.end();
 });
 
